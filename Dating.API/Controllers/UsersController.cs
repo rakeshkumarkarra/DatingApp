378815687f4e86114bc19845d8517a17083a8761
rakeshkumarkarra <rakeshkumarkarra@hotmail.com>
2021-08-17 01:32:42 +0530

    Initial api commit

---
+++ Dating.API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Dating.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dating.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DatingAppContext _context;
+
+        public UsersController(DatingAppContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await Task.FromResult(await _context.AppUsers.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await Task.FromResult(await _context.AppUsers.FindAsync(id));
+        }
+    }
+}
---
+++ Dating.API/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Dating.API.Models
+{
+    public partial class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
